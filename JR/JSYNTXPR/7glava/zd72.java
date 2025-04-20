724f13252185df9d644fab1ab74a32b60ffd2ba2
kol4egraf0-0 <genitya408@gmail.com>
2025-04-20 16:01:09 +0300

    funs and tipos

---
+++ JR/JSYNTXPR/7glava/zd72.java
@@ -0,0 +1,12 @@
+public class zd72 {
+    public static void main(String[] args) {
+
+    }
+
+    public static long cube(long a){
+        return a*a*a;
+    }
+    public static long ninthDegree(long a){
+        return cube(cube(a));
+    }
+}
---
+++ JR/JSYNTXPR/8glava/zd8.java
@@ -0,0 +1,6 @@
+public class zd8 {
+    long a = 109 + 15;
+    int b = (int) a * 2;
+    short c = (short) (a / b);
+    byte d = (byte) (a + b - c);
+}

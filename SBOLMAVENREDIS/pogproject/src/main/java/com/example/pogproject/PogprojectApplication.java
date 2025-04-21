982e8be65a36b9afc18b7f20ef17a7a989e57ab8
kol4egraf0-0 <genitya408@gmail.com>
2025-04-21 23:02:15 +0300

    renaiming and init ub+redis pr

---
+++ SBOLMAVENREDIS/pogproject/src/main/java/com/example/pogproject/PogprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pogproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PogprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PogprojectApplication.class, args);
+	}
+
+}
---
+++ SBOLMAVENREDIS/pogproject/src/test/java/com/example/pogproject/PogprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pogproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PogprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b87546dc344ebd8d378bdf9a0dac567f3b6e08b9
kol4egraf0-0 <genitya408@gmail.com>
2025-04-21 13:23:13 +0300

    http 2nd @value

--- SBOLMAVEN/demo/src/main/java/com/demooo/demo/Controller/GreetingController.java
+++ SBOLMAVEN/demo/src/main/java/com/demooo/demo/Controller/GreetingController.java
@@ -8,11 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
-    @Value("${greeting-name: HELLO}")
+    @Value("${greeting-name: JEKI4}")
     private String name;
     
+    @Value("${greeting-coffee: ${greeting-name} is drink coca-colas")
+    private String coffee;
+
     @GetMapping
     String getGreeting(){
         return name;
     }
+
+    @GetMapping("/coffee")
+    String getNameAndCoffee(){
+        return coffee;
+    }
+
 }

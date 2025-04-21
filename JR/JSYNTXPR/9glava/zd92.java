febf48d307937712060ae677d22cd19396900dab
kol4egraf0-0 <genitya408@gmail.com>
2025-04-21 17:58:03 +0300

    9glava half mb not half

---
+++ JR/JSYNTXPR/9glava/zd92.java
@@ -0,0 +1,12 @@
+
+/*
+importes very cool full!!!
+*/
+
+public class zd92 {
+    public static void main(String[] args) {
+        java.util.ArrayList<java.lang.String> arrayList = new java.util.ArrayList<>();
+        java.io.BufferedReader bufferedReader = new java.io.BufferedReader(new java.io.StringReader(""));
+        java.math.BigInteger bigInteger = new java.math.BigInteger("123");
+    }
+}       
---
+++ JR/JSYNTXPR/9glava/zd93.java
@@ -0,0 +1,15 @@
+public class zd93 {
+
+    public static void main(String[] args) throws InterruptedException {
+        printNumbers();
+    }
+
+    public static void printNumbers() throws InterruptedException {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+            if (i < 10) {
+                Thread.sleep(1, 300_000);
+            }
+        }
+    }
+}

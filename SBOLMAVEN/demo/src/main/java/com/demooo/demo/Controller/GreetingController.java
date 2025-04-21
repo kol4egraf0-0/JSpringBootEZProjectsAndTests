d717c20c158bd7f03b800821afc00d575e25a508
kol4egraf0-0 <genitya408@gmail.com>
2025-04-21 13:54:19 +0300

    @ConfigurationProp check

--- SBOLMAVEN/demo/src/main/java/com/demooo/demo/Controller/GreetingController.java
+++ SBOLMAVEN/demo/src/main/java/com/demooo/demo/Controller/GreetingController.java
@@ -1,27 +1,28 @@
 package com.demooo.demo.Controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.demooo.demo.Properties.Greeting;
+
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
-    @Value("${greeting-name: JEKI4}")
-    private String name;
     
-    @Value("${greeting-coffee: ${greeting-name} is drink coca-colas")
-    private String coffee;
+    private final Greeting greeting;
 
+    public GreetingController(Greeting greeting){
+        this.greeting = greeting;
+    }
+    
     @GetMapping
-    String getGreeting(){
-        return name;
+    String getGreeting() {
+        return greeting.getName();
     }
 
     @GetMapping("/coffee")
-    String getNameAndCoffee(){
-        return coffee;
+    String getNameAndCoffee() {
+        return greeting.getCoffee();
     }
-
 }
--- SBOLMAVEN/demo/src/main/java/com/demooo/demo/DemoApplication.java
+++ SBOLMAVEN/demo/src/main/java/com/demooo/demo/DemoApplication.java
@@ -2,7 +2,8 @@ package com.demooo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class DemoApplication {
 

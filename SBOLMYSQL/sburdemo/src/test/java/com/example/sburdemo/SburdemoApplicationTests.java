68cc4632824915ef077eaebf22b96b41562250fa
kol4egraf0-0 <genitya408@gmail.com>
2025-04-22 13:36:04 +0300

    repo+entity

---
+++ SBOLMYSQL/sburdemo/src/test/java/com/example/sburdemo/SburdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sburdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SburdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

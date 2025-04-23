f3993d595564b8725ea526d1341e7f774fab682f
kol4egraf0-0 <genitya408@gmail.com>
2025-04-23 14:27:20 +0300

    add repo

---
+++ SBOLNEO4JGROOVY/sbur-neoj/src/main/java/com/example/sbur_neoj/AircraftRepository.java
@@ -0,0 +1,6 @@
+package com.example.sbur_neoj;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AircraftRepository extends CrudRepository<Aircraft, Long> { //PagingAndSortingRep для нео4джи!!!
+}

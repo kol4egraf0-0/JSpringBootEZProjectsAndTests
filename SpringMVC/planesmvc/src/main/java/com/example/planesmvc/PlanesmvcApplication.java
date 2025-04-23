1bbd8fe9969200c60908e8702005eb92e274af14
kol4egraf0-0 <genitya408@gmail.com>
2025-04-23 17:53:52 +0300

    init spring mvc project

---
+++ SpringMVC/planesmvc/src/main/java/com/example/planesmvc/PlanesmvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.planesmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanesmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanesmvcApplication.class, args);
+	}
+
+}
---
+++ SpringMVC/planesmvc/src/test/java/com/example/planesmvc/PlanesmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planesmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanesmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

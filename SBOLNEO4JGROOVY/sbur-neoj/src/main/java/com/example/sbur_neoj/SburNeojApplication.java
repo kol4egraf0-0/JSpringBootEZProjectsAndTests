f224685725fe9b0a1661489030662a98156afacb
kol4egraf0-0 <genitya408@gmail.com>
2025-04-23 14:15:05 +0300

    init neo4j project+groovy sp

---
+++ SBOLNEO4JGROOVY/sbur-neoj/src/main/java/com/example/sbur_neoj/SburNeojApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbur_neoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SburNeojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SburNeojApplication.class, args);
+	}
+
+}

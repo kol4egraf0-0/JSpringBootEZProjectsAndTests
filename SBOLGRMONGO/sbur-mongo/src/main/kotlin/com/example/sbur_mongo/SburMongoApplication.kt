7d2338670fc43c64b3af99a57fd2043503909c17
kol4egraf0-0 <genitya408@gmail.com>
2025-04-23 09:52:54 +0300

    init sp+mongo(embedded for test in app)

---
+++ SBOLGRMONGO/sbur-mongo/src/main/kotlin/com/example/sbur_mongo/SburMongoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sbur_mongo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SburMongoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SburMongoApplication>(*args)
+}

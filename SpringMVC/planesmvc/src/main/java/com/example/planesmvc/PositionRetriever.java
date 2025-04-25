b66c1964c7db9136d05f316f01b14e777e2100fe
kol4egraf0-0 <genitya408@gmail.com>
2025-04-25 17:59:44 +0300

    add ws config + fix poly4atel.class

--- SpringMVC/planesmvc/src/main/java/com/example/planesmvc/PositionRetriever.java
+++ SpringMVC/planesmvc/src/main/java/com/example/planesmvc/PositionRetriever.java
@@ -3,6 +3,8 @@ package com.example.planesmvc;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 
 import java.util.List;
 import java.util.function.Consumer;
@@ -11,6 +13,7 @@ import java.util.function.Consumer;
 @Configuration
 public class PositionRetriever {
     private final AircraftRepository repo;
+    private final WebSocketHandler handler;
     @Bean
     Consumer<List<Aircraft>> retrieveAircraftPositions() {
      return acList -> {
@@ -21,5 +24,20 @@ public class PositionRetriever {
 
        repo.findAll().forEach(System.out::println);//проверка
      };
+    }
+
+    private void sendPositions() {
+        if(repo.count() > 0) {
+            for(WebSocketSession sessionInList : handler.getSessionList()){
+                try{
+                    sessionInList.sendMessage(
+                            new TextMessage(repo.findAll().toString())
+                    );
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }

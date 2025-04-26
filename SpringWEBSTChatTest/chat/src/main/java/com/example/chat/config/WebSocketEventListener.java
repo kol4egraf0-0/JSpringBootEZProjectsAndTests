8d2de0eebd23e27163c86f11d15a48c56810b59b
kol4egraf0-0 <genitya408@gmail.com>
2025-04-26 17:29:28 +0300

    eventlistener update

--- SpringWEBSTChatTest/chat/src/main/java/com/example/chat/config/WebSocketEventListener.java
+++ SpringWEBSTChatTest/chat/src/main/java/com/example/chat/config/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package com.example.chat.config;
 
+import com.example.chat.chat.ChatMessage;
+import com.example.chat.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -11,8 +15,20 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessagingTemplate MESSAGING_TEMPLATE;
+
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
         //.
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) accessor.getSessionAttributes().get("username");
+        if(username != null) {
+            log.info("пользователь отключился: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            MESSAGING_TEMPLATE.convertAndSend("/topic/public", chatMessage);
+        }
     }
 }

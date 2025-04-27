f6cd9ef2ce14fa782705938990fbb6abf8a60700
kol4egraf0-0 <genitya408@gmail.com>
2025-04-27 10:55:16 +0300

    fix bugs and app r

--- SpringReactiveProgramming/planefinder/src/main/java/com/thehecklers/planefinder/DbConxInit.java
+++ SpringReactiveProgramming/planefinder/src/main/java/com/thehecklers/planefinder/DbConxInit.java
@@ -4,10 +4,12 @@ import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
+@Configuration
 public class DbConxInit {
     @Bean
     public ConnectionFactoryInitializer initializer(@Qualifier("connectionFactory") ConnectionFactory connectionFactory) {

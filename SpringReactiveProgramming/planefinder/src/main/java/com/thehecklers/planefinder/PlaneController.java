29cceb217d903c4e9ed832c76a45b95ed5302552
kol4egraf0-0 <genitya408@gmail.com>
2025-04-27 12:05:29 +0300

    update for rsocket from http

--- SpringReactiveProgramming/planefinder/src/main/java/com/thehecklers/planefinder/PlaneController.java
+++ SpringReactiveProgramming/planefinder/src/main/java/com/thehecklers/planefinder/PlaneController.java
@@ -1,11 +1,13 @@
 package com.thehecklers.planefinder;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import reactor.core.publisher.Flux;
 
 import java.io.IOException;
+import java.time.Duration;
 
 @Controller
 public class PlaneController {
@@ -19,5 +21,12 @@ public class PlaneController {
     @GetMapping("/aircraft")
     public Flux<Aircraft> getCurrentAircraft() throws IOException {
         return pfService.getAircraft();
+    }
+
+    @MessageMapping("acstream") //НЕ НУЖУЕ СЛЕШ!!!
+    public Flux<Aircraft> getCurrentACStream() throws IOException {
+        return pfService.getAircraft().concatWith(
+                Flux.interval(Duration.ofSeconds(1))
+                        .flatMap(i -> pfService.getAircraft()));
     }
 }

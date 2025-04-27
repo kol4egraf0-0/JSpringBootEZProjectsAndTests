858b8a3bb52706ff9147ea53621c5ec47ba4ac6c
kol4egraf0-0 <genitya408@gmail.com>
2025-04-27 12:40:04 +0300

    rscoket to client app full work

--- SpringReactiveProgramming/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/PositionController.java
+++ SpringReactiveProgramming/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/PositionController.java
@@ -2,20 +2,30 @@ package com.thehecklers.aircraftpositions;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
-@RequiredArgsConstructor
 @Controller
 public class PositionController {
     @NonNull
     private final AircraftRepository repository;
+    private final RSocketRequester requester;
     private WebClient client =
             WebClient.create("http://localhost:7634/aircraft");
 
+    public PositionController(AircraftRepository repository, RSocketRequester.Builder builder) { //убрал requArgCon анотацию
+        this.repository = repository;
+        this.requester = builder.tcp("localhost", 7635);
+    }
+
+
+    //конечная точка http
     @GetMapping("/aircraft")
     public String getCurrentAircraftPositions(Model model) {
         Flux<Aircraft> aircraftFlux = repository.deleteAll()
@@ -26,5 +36,15 @@ public class PositionController {
 
         model.addAttribute("currentPositions", aircraftFlux);
         return "positions";
+    }
+
+    //клиентаская точка rsocket
+    @ResponseBody //json
+    // produces = MediaType.TEXT_EVENT_STREAM_VALUE - указывает, что ответ будет потоком серверных событий (Server-Sent Events) SSE бля
+    @GetMapping(value = "/acstream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Aircraft> getCurrentACPositionsStream() {
+        return requester.route("acstream")
+                .data("Requesting aircraft positions")
+                .retrieveFlux(Aircraft.class);
     }
 }

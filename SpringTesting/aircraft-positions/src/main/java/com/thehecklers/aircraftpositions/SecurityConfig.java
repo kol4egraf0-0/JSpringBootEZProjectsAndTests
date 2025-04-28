01dfdfd6494d4e01df59e640f5218248bba99625
kol4egraf0-0 <genitya408@gmail.com>
2025-04-28 15:12:34 +0300

    add auth0 w/o okta

--- SpringTesting/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/SecurityConfig.java
+++ SpringTesting/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/SecurityConfig.java
@@ -2,21 +2,33 @@ package com.thehecklers.aircraftpositions;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Configuration     //@EnableWebSecurity conf+ webaleglobaluth
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(auth -> auth
+                        .antMatchers("/aircraft", "/aircraftadmin").permitAll()  // для всех чисто для теста
+                        .anyRequest().authenticated()  // Остальное — только для аутентифицированных
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/oauth2/authorization/auth0")  // Перенаправление на Auth0
+                );
+    }
 
     @Bean
     WebClient client(ClientRegistrationRepository regRepo,
                      OAuth2AuthorizedClientRepository cliRepo) {
         ServletOAuth2AuthorizedClientExchangeFilterFunction filter =
                 new ServletOAuth2AuthorizedClientExchangeFilterFunction(regRepo, cliRepo);
-
         filter.setDefaultOAuth2AuthorizedClient(true);
 
         return WebClient.builder()
@@ -24,5 +36,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .apply(filter.oauth2Configuration())
                 .build();
     }
-
 }

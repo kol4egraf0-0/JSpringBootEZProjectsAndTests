71bd59572b78c82244785c51a5990ab5053b874a
kol4egraf0-0 <genitya408@gmail.com>
2025-04-29 09:43:30 +0300

    add secvice and refactor 2

--- SpringReactiveProgAndTesting/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/PositionController.java
+++ SpringReactiveProgAndTesting/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/PositionController.java
@@ -11,27 +11,20 @@ import reactor.core.publisher.Flux;
 
 @Controller
 public class PositionController {
-    private final AircraftRepository repository;
+    private final PositionService service;
     private final RSocketRequester requester;
     private WebClient client =
             WebClient.create("http://localhost:7634/aircraft");
 
-    public PositionController(AircraftRepository repository, RSocketRequester requester) {
-        this.repository = repository;
+    public PositionController(PositionService service, RSocketRequester requester) {
+        this.service = service;
         this.requester = requester;
     }
 
     // HTTP endpoint, HTTP requester (previously created)
     @GetMapping("/aircraft")
     public String getCurrentAircraftPositions(Model model) {
-        Flux<Aircraft> aircraftFlux = repository.deleteAll()
-                .thenMany(client.get()
-                        .retrieve()
-                        .bodyToFlux(Aircraft.class)
-                        .filter(plane -> !plane.getReg().isEmpty())
-                        .flatMap(repository::save));
-
-        model.addAttribute("currentPositions", aircraftFlux);
+        model.addAttribute("currentpositions", service.getAllAircraft());
         return "positions";
     }
 
@@ -40,7 +33,7 @@ public class PositionController {
     @GetMapping(value = "/acstream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Aircraft> getCurrentACPositionsStream() {
         return requester.route("acstream")
-                .data("Requesting aircraft positions")
+                .data("Ищем позиции самолетов")
                 .retrieveFlux(Aircraft.class);
     }
 }

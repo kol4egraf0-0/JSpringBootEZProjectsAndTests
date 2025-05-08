4e3f39e45384b4796374a0d2d09482e947a16549
kol4egraf0-0 <genitya408@gmail.com>
2025-05-08 15:12:45 +0300

    add app.jsx+css+fix

--- SpringMYSQL/productAppDINOforM/frontend/src/App.jsx
+++ SpringMYSQL/productAppDINOforM/frontend/src/App.jsx
@@ -1,34 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import ProductList from './ProductList';
+import CategoryFilter from './CategoryFilter';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory]  = useState(null); //не массив а значение поэтому null
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
+
+  useEffect(()=>{
+    fetch('http://localhost:8080/api/products')
+    .then(response => response.json())
+    .then(data => setProducts(data));
+
+    fetch('http://localhost:8080/api/categories')
+    .then(response => response.json())
+    .then(data => setCategories(data));
+  }, [])
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const handleSortChange = (event) => {
+    setSortOrder(event.target.value)
+  }
+
+  const handleCategorySelect = (categoryId) => {
+    setSelectedCategory(categoryId ? Number(categoryId) : null)
+  }
+
+  const filterProducts = products
+        .filter(product => {
+          return (
+            (selectedCategory ? product.categoryId === selectedCategory : true)
+          &&
+          product.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+        })
+        .sort((a,b)=>{
+          if(sortOrder === "asc"){
+            return a.price - b.price; //от а до б 
+          }
+          else{
+            return b.price - a.price;
+          }
+        });
 
   return (
-    <>
+    <div className='container'>
+      <h1 className='my-4'>Каталог продуктов</h1>
+      <div className='row align-items-center mb-4'>
+        <div className='col-md-3 col-sm-12 mb-2'>
+          <CategoryFilter categories={categories} onSelect={handleCategorySelect}/>
+        </div>
+        <div className='col-md-5 col-sm-12 mb-2'>
+          <input
+          type='text'
+          className='form-control'
+          placeholder='Поиск по продуктам'
+          onChange={handleSearchChange}
+          />
+        </div>
+
+        <div className='col-md-4 col-sm-1 mb-2'>
+          <select className='form-control'
+          onChange={handleSortChange}>
+            <option value="asc">Сортировка по Цене: По Возрастанию</option>
+            <option value="desc">Сортировка по Цене: По Убыванию</option>
+          </select>
+        </div>
+      </div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    {filterProducts.length ? ( //публикация продуктов
+      <ProductList
+      products={filterProducts}/>
+    ) : (
+      <p>Продукты не найдены</p>
+    )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>  
   )
 }
 
